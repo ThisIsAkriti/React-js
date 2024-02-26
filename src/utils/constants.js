2452d70a3b69f491f86865188fe458fc092b6287
Akriti <akritikohli05@gmail.com>
2024-02-26 11:37:01 +0530

    added cors proxy to menu api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const LOGO_URL = "https://th.bing.com/th/id/OIP.wxzLjpLBORthHvGLBzQUYwHaH
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+export const MENU_API = "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
 
 //export const MENU_URL =   
 //"https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D15.8323892%26lng%3D78.0544946%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING"
